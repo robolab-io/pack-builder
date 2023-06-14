f35702d9f3ab27331e4513b8de86081edafe931a
Thomas Card <tcardlab@gmail.com>
2023-06-14 02:54:51 -0400

    Init: vue auto-import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    AutoImport({
+      imports: ['vue', '@vueuse/core'],
+      dirs: [
+        './composables/**',
+      ],
+      vueTemplate: true,
+      cache: true,
+    }),
+    vue()
+  ],
 })
